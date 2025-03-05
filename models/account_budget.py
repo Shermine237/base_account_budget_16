6c219b51854e689ae3bce480efe70c8bd258b0f8
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2025-03-05 10:35:27 +0100

    [FIX] WARNING odoo-db odoo.api.create: The model odoo.addons.base_account_budget_16.models.account_budget is not overriding the create method in batch

--- models/account_budget.py
+++ models/account_budget.py
@@ -75,13 +75,9 @@ class Budget(models.Model):
     company_id = fields.Many2one('res.company', 'Company', required=True,
                                  default=lambda self: self.env.company)
 
-    @api.model
+    @api.model_create_multi
     def create(self, vals_list):
-        if not vals_list:
-            return self.browse()
-        if isinstance(vals_list, (dict,)):
-            vals_list = [vals_list]
-        return super(models.Model, self).create(vals_list)
+        return super().create(vals_list)
 
     def action_budget_confirm(self):
         self.write({'state': 'confirm'})
